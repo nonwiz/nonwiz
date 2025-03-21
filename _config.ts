32fbbb02eac4674b6ab2e175422ae3fa754ee5c6
nonwiz <nonwiz@pm.me>
2025-03-21 17:52:07 +0700

    feat: update for asset paths and link handling

--- _config.ts
+++ _config.ts
@@ -4,33 +4,25 @@ site.copy("assets");
 site.copy("style.css");
 site.copy("main.js");
 
-const BASE_PATH = "/" as const;
-
-function prependBasePathToAssets(content: string, basePath: string) {
-  return content.replace(/(src|href)="\/?([^"]+)"/g, (_, attr, path) => {
-    return `${attr}="${basePath}${path.replace(/^\/?/, "")}"`;
-  });
-}
-
-function convertAllExternalLinkToNewTab(content: string) {
-  return content.replace(/(href=")([^"]+)(")/g, (_, prefix, url, suffix) => {
-    return `${prefix}${url} ${suffix} ${
-      url.includes("http") ? 'target="_blank"' : ""
-    }`;
-  });
-}
-
-function parseObsidianImages(content: string) {
-  return content.replace(/!\[\[(.*?)\]\]/g, (_, filename) => {
-    const cleanFilename = filename.replace(/^assets\//, "");
-    return `<img src="/${encodeURIComponent(cleanFilename)}">`;
-  });
-}
+const BASE_PATH = "" as const;
 
 function parseObsidian(content: string) {
-  return prependBasePathToAssets(
-    convertAllExternalLinkToNewTab(parseObsidianImages(content)),
-    BASE_PATH
+  return prependBasePathToAssets(content, BASE_PATH);
+}
+
+function prependBasePathToAssets(content: string, basePath: string) {
+  return content.replace(
+    /(src|href)="\/?([^"]+)"/g,
+    (_, attr, path: string) => {
+      path = path.trim();
+      if (path.startsWith("http")) {
+        return `${attr}="${path}" target="_blank"`;
+      }
+      if (path.endsWith(".md")) {
+        return `${attr}="${basePath}/${path.replace(/\.md$/, "")}"`;
+      }
+      return `${attr}="${basePath}/${path}"`;
+    }
   );
 }
 
